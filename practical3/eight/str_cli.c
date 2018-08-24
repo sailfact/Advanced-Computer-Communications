161c75a27fb8a81477f73943d03ea992c8cd535b
sailfact <sailfact@outlook.com>
2018-08-24 22:50:43 +0800

    str_cli

--- practical3/eight/str_cli.c
+++ practical3/eight/str_cli.c
@@ -1,23 +1,18 @@
 #include	"acc.h"
 
-void str_cli(FILE *od, int sockfd)
+void str_cli(FILE *fp, int sockfd)
 {
 	char	sendline[MAXLINE], recvline[MAXLINE];
-	FILE 	*fp;
-	int 	rc;
+	FILE *newFile;
 
-	while ((void *) Fgets(sendline, MAXLINE, od) != NULL) {	// user input
-		Writen(sockfd, sendline, strlen(sendline));	// write input to socket
-		if ((fp = fopen(sendline, "w")) != NULL) {	// open file
-			while ((rc = Readline(sockfd, recvline, MAXLINE))== 0) { // read from socket
-				if (rc < 0) {
-					err_quit("str_cli: server terminated prematurely");
-				}
-				if (fp!=NULL) {
-					fprintf(fp,recvline); // write line to file
-				}
-			}
-			fclose (fp);
-		}
+	while ((void *) Fgets(sendline, MAXLINE, fp) != NULL) {
+		newFile = fopen(sendline, "w");
+
+		Writen(sockfd, sendline, strlen(sendline));
+
+		if (Readline(sockfd, recvline, MAXLINE) == 0)
+			err_quit("str_cli: server terminated prematurely");
+
+
 	}
 }
