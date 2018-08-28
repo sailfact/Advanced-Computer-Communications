36dc1e7c92122e6af736ffdf537b35990fa94213
sailfact <sailfact@outlook.com>
2018-08-28 15:45:00 +0800

    started prac 4

--- practical3/Q8/str_file.c
+++ practical3/Q8/str_file.c
@@ -1,21 +1,22 @@
 #include	"acc.h"
 
-void
-str_file(int sockfd)
+void str_file(int sockfd)
 {
 	ssize_t		n;
 	char		line[MAXLINE];
 	char		buffer[MAXLINE];
+	const char 	*filename;
 	int 		inFile, n_char = 0;
 
 	for ( ; ; ) {
 		if ( (n = Readline(sockfd, line, MAXLINE)) == 0)
 			return;		/* connection closed by other end */
 		printf("%s", line);
+		bcopy(line, filename, sizeof(line));
 
-		if ((inFile=open("test.txt",O_RDONLY)) != -1) {
-			while((n_char=read(inFile, buffer, MAXLINE)) != 0) {
-				n_char=write(sockfd,buffer,n_char);
+		if ((inFile=open(line, O_RDONLY)) != -1) {
+			while((n_char=Readline(inFile, buffer, MAXLINE)) != 0) {
+				n_char=Writen(sockfd,buffer,n_char);
 				// Writen(sockfd, buffer, n_char);
 			}
 			Close(inFile);
