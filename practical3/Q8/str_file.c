9aedf7be285a3567d48085fd23813abb09f67889
sailfact <sailfact@outlook.com>
2018-08-28 16:20:04 +0800

    q8

--- practical3/Q8/str_file.c
+++ practical3/Q8/str_file.c
@@ -2,26 +2,24 @@
 
 void str_file(int sockfd)
 {
-	ssize_t		n;
+	ssize_t		n, read;
 	char		line[MAXLINE];
 	char		buffer[MAXLINE];
-	const char 	*filename;
-	int 		inFile, n_char = 0;
+	FILE  		*fp;
 
 	for ( ; ; ) {
 		if ( (n = Readline(sockfd, line, MAXLINE)) == 0)
 			return;		/* connection closed by other end */
-		printf("%s", line);
-		bcopy(line, filename, sizeof(line));
-
-		if ((inFile=open(line, O_RDONLY)) != -1) {
-			while((n_char=Readline(inFile, buffer, MAXLINE)) != 0) {
-				n_char=Writen(sockfd,buffer,n_char);
-				// Writen(sockfd, buffer, n_char);
-			}
-			Close(inFile);
-		}
-		else
+		printf(">%s", line);
+		
+		fp = fopen("test.txt", "r");
+		if (fp == NULL)
 			return;
+
+		while ((read = getline(buffer, MAXLINE, fp)) != -1) {
+			printf("%s", buffer);
+			Writen(sockfd, buffer, MAXLINE);
+		}
+		Fclose(fp);
 	}
 }
